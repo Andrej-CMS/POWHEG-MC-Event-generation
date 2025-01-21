acdf5992af463a49a63e1e7680dcdeb91ec9e4fd
Andrej <asaibel@cern.ch>
2025-01-21 10:33:04 +0100

    update setup scripts for POWHEG EL9 and TIRANT

---
+++ POWHEG_Files/setup_EL9lxplus.sh
@@ -0,0 +1,6 @@
+setupATLAS
+asetup AthGeneration,23.6.40
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/eos/user/a/asaibel/PowhegTTbarj/fastjet-install/lib:/eos/user/a/asaibel/PowhegTTbarj/LHAPDF-6.5.4-install/lib
+
+export PATH=$PATH:/eos/user/a/asaibel/PowhegTTbarj/fastjet-install/bin:/eos/user/a/asaibel/PowhegTTbarj/LHAPDF-6.5.4-install/bin
